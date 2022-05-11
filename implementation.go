47aa226d754c27bd73ff1167d271ae2dc0f4b40a
Ivan Labiak <labyak.ivan2003@gmail.com>
2022-05-11 16:14:20 +0300

    made function to calculate postfix evaluation

--- implementation.go
+++ implementation.go
@@ -1,9 +1,65 @@
-package lab2
+package main //package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
 // TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+// Calculate postfix expression
+func CalculatePostfix(input string) (string, error) {
+	var stack []float64
+	var chars = strings.Fields(input)
+	var value float64
+	for _, char := range chars {
+		switch char {
+		case "+", "-", "*", "/", "^":
+			var firstVal, secondVal float64
+			if len(stack) < 2 {
+				return "0", fmt.Errorf("There are no 2 values, can't calculate expression - %s", char)
+			}
+			stack, firstVal, secondVal = GetValuesFromStack(stack)
+			switch char {
+			case "+":
+				value = firstVal + secondVal
+			case "-":
+				value = firstVal - secondVal
+			case "*":
+				value = firstVal * secondVal
+			case "/":
+				value = firstVal / secondVal
+			case "^":
+				value = math.Pow(firstVal, secondVal)
+			}
+		default:
+			var err error
+			if value, err = strconv.ParseFloat(char, 64); err != nil {
+				return "0", fmt.Errorf("Wrong value - %s", char)
+			}
+		}
+		stack = append(stack, value)
+	}
+	if len(stack) != 1 {
+		return "0", fmt.Errorf("Wrong expression - %s", stack)
+	}
+	str := fmt.Sprintf("%f", stack[0])
+	return str, nil
+}
+
+
+func GetValuesFromStack(stack []float64) ([]float64, float64, float64) {
+	var values []float64
+	stack, values = stack[:len(stack)-2], stack[len(stack)-2:]
+	//fmt.Println(values[0], values[1])
+	return stack, values[0], values[1]
+}
+
+func main() {
+	fmt.Println(CalculatePostfix("0")) 
+	fmt.Println(CalculatePostfix("1 2 + 6 - 2 *"))
+	fmt.Println(CalculatePostfix("1 2 6 * + 3 +"))
+	fmt.Println(CalculatePostfix("2 5 ^"))
+	fmt.Println(CalculatePostfix(""))
 }
--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,23 @@
 package lab2
 
 import (
-	"fmt"
-	"testing"
+	//"fmt"
+	//"testing"
 
-	"github.com/stretchr/testify/assert"
+	//"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+/* func TestPrefixToPostfix(t *testing.T) {
+	res, err := CalculatePostfix("+ 5 * - 4 2 3")
 	if assert.Nil(t, err) {
 		assert.Equal(t, "4 2 - 3 * 5 +", res)
 	}
-}
+} */
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+/* func ExamplePrefixToPostfix() {
+	res, _ := CalculatePostfix("+ 2 2")
 	fmt.Println(res)
 
 	// Output:
 	// 2 2 +
-}
+} */
