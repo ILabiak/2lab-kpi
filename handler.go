a3957079c1fae88b8ef02bd41c84e5503d271910
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-05-25 21:30:47 +0300

    ComputeHandler implemented

--- handler.go
+++ handler.go
@@ -1,12 +1,30 @@
 package lab2
 
+import (
+	"io"
+)
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	Input  io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
+	var buf []byte
+	var (
+		result string
+		err    error
+	)
+	ch.Input.Read(buf)
+	result, err = CalculatePostfix(string(buf))
+	if result == "Nil" && err != nil {
+		ch.Output.Write([]byte(err.Error()))
+	} else if result != "Nil" {
+		ch.Output.Write([]byte(result))
+	} else {
+		return err
+	}
 	return nil
 }
